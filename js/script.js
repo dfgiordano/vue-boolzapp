22c5a53f026db6950a99a8fa252568acff4d41a9
dfgiordano <federico.giordano90@gmail.com>
2021-05-25 17:24:59 +0200

    milestone 3 completata

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 // Milestone 4
 // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 var app = new Vue (
@@ -105,7 +103,7 @@ var app = new Vue (
             },
             activeContact : function (index) {
                 this.activeIndex = index;
-                this.selectedChat = this.getCurrentChat();
+                this.selectedChat = this.getCurrentChat(); 
             },
             getLastMessageData: function (index) {
                 return this.contacts[index].messages[this.contacts[index].messages.length -1].date;
@@ -133,8 +131,17 @@ var app = new Vue (
                     text: this.newMessage,
                     status: 'sent'
                 });
+
+                setTimeout(() => {
+                    this.getCurrentChat().messages.push({
+                        date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                        text: 'ok',
+                        status: 'received'
+                    });
+                }, 1000)
                 this.newMessage= "";
-            }
+            },
+            
         }
     }
 );
